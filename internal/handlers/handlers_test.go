ce6e382549132c515ac424d17eeac174c005b82e
Nikolay Yakubitskiy <steseagal87@gmail.com>
2022-09-17 17:31:22 +0300

    remove unnecessary

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -3,7 +3,6 @@ package handlers
 import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -26,15 +25,6 @@ var shortUrls = []struct {
 	{"http://localhost:8080/GJ"},
 	{"http://localhost:8080/vX"},
 	{"http://localhost:8080/78"},
-}
-
-type RepoTool interface {
-	Set(short string, fullUrl string) (id int64, err error)
-	Get(short string) (fullUrl string, err error)
-}
-
-type MockRepo struct {
-	mock.Mock
 }
 
 func TestShortHandler(t *testing.T) {
@@ -71,7 +61,6 @@ func TestGetFull(t *testing.T) {
 			fmt.Sprintf("/v1/shorturl/full?shorturl=%s", e.url), nil)
 
 		handler.GetFull(w, req)
-		//mockCall.Unset()
 		res := w.Result()
 		resBody, _ := io.ReadAll(res.Body)
 
@@ -79,11 +68,4 @@ func TestGetFull(t *testing.T) {
 		assert.Contains(t, string(resBody), "Moved Permanently")
 
 	}
-}
-
-func (mr *MockRepo) Set(short string, fullUrl string) (id int64, err error) {
-	return
-}
-func (mr *MockRepo) Get(short string) (fullUrl string, err error) {
-	return
 }
